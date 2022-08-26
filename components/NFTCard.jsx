c06c4b9af285a1f54c20f015be666211dbfd151d
Mario <mario@isla-miablockchain.solutions>
2022-08-26 08:56:41 -0400

    bug fix nft images not showing

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 
 import images from '../assets';
@@ -13,7 +12,7 @@ const NFTCard = ({ nft, onProfilePage }) => {
     <Link href={{ pathname: '/nft-details', query: nft }}>
       <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md">
         <div className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
-          <Image src={nft.image || images[`nft${nft.i}`]} layout="fill" objectFit="cover" alt="nft01" />
+          <img src={nft.image || images[`nft${nft.i}`]} width={200} objectFit="cover" alt="nft01" />
         </div>
         <div className="mt-3 flex flex-col">
           <p className="font-roboto dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{nft.name}</p>
