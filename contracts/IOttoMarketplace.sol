e7992956aedb133fd2d0cbaa7bd5a58ffe597714
Mario <mario@isla-miablockchain.solutions>
2022-10-29 18:37:55 -0400

    IOttoMarketplace check in

---
+++ contracts/IOttoMarketplace.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IOttoMarketplace {
+    struct MarketItem {
+      bytes32 tokenSeller;
+      uint256 tokenId;
+      address payable seller;
+      address payable owner;
+      uint256 price;
+      uint256 amount;
+      uint256 sold;
+    }    
+}
