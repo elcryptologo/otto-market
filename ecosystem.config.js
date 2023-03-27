7554d0cbdeb9ac2380585310ba637153ca2a864a
Rolando Gil <98071118+elcryptologo@users.noreply.github.com>
2023-03-27 13:11:37 -0400

    Add files via upload
    Signed-off-by: Rolando Gil <98071118+elcryptologo@users.noreply.github.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,10 @@
 module.exports = {
-  apps: [{
-    script: 'index.js',
-    watch: '.',
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker'],
-  }],
-
-  deploy: {
-    production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
+  apps: [
+    {
+      name: "next-app",
+      script: "./node_modules/next/dist/bin/next",
+      watch: false,
+      autorestart: true,
     },
-  },
+  ],
 };
