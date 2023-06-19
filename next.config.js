446997bc54216a1e805b97971ef885cdd1753c3e
mario00677 <mario00677@gmail.com>
2023-06-19 16:36:00 -0400

    the untract files mario-otto

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+module.exports = {
+  images: {
+    domains: ['gateway.pinata.cloud'],
+  },
+  assetDir: 'assets',
+  env: {
+    pinAuth: process.env.PINAUTH,
+    tmdbKey: process.env.TMDBKEY,
+    tmdbApiHost: 'https://api.themoviedb.org',
+    tmdbHost: 'https://www.themoviedb.org',
+    currency: 'MATIC',
+  },
+};
